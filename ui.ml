244abaf582dff4d7c3c7929c2e57eeec4ad1645e
Benjamin LECHIARA <blechiar@e2r13p13.42.fr>
2015-06-28 09:50:39 +0200

    Add event animation

--- ui.ml
+++ ui.ml
@@ -1,8 +1,6 @@
 class ui (pet: Pet.pet) =
   object (self)
 
-    val mutable petImage = GMisc.image ()
-    
     val mutable window          = GWindow.window ()
     val mutable vbox            = GPack.vbox ()
     val mutable sbox            = GPack.hbox ()
@@ -11,18 +9,19 @@ class ui (pet: Pet.pet) =
     val mutable stat_energy     = GRange.progress_bar ()
     val mutable stat_hygiene    = GRange.progress_bar ()
     val mutable stat_happy      = GRange.progress_bar ()
-
+    val mutable petImage        = GMisc.image ()
+    val mutable _action         = false
 
     method init =
 
       ignore (GtkMain.Main.init ());
       window <- GWindow.window ~border_width:10
-                    ~width:200
+                    ~width:250
                     ~height:200
                     ~title:"Instant Tama" ();
       vbox <- GPack.vbox ~packing:window#add ();
       sbox <- GPack.hbox ~packing:vbox#add ();
-      pbox <- GPack.hbox ~height:50 ~width:200 ~packing:vbox#add ();
+      pbox <- GPack.hbox ~height:100 ~width:250 ~packing:vbox#add ();
 
       self#createStatBar ;
       self#createPetZone ;
@@ -62,12 +61,21 @@ class ui (pet: Pet.pet) =
       let button_thunder = GButton.button ~label:"THUNDER" ~packing:bbox#add () in
       let button_bath = GButton.button ~label:"BATH" ~packing:bbox#add () in
       let button_kill = GButton.button ~label: "KILL" ~packing:bbox#add () in
+      ignore (button_eat#connect#clicked     ~callback: ((fun () -> if _action = false then begin _action <- true; self#anim; pet#eat;     self#drawImage ((pet#get_sprite "eat"));     self#refresh end)));
+      ignore (button_thunder#connect#clicked ~callback: ((fun () -> if _action = false then begin _action <- true; self#anim; pet#thunder; self#drawImage ((pet#get_sprite "thunder")); self#refresh end)));
+      ignore (button_bath#connect#clicked    ~callback: ((fun () -> if _action = false then begin _action <- true; self#anim; pet#bath;    self#drawImage ((pet#get_sprite "bath"));    self#refresh end)));
+      ignore (button_kill#connect#clicked    ~callback: ((fun () -> if _action = false then begin _action <- true; self#anim; pet#kill;    self#drawImage ((pet#get_sprite "kill"));    self#refresh end)));
 
 
-      ignore (button_eat#connect#clicked ~callback:((fun () -> pet#eat; self#drawImage ((pet#get_sprite "eat")); self#refresh )));
-      ignore (button_thunder#connect#clicked ~callback:((fun () -> pet#thunder; self#drawImage ((pet#get_sprite "thunder")); self#refresh )));
-      ignore (button_bath#connect#clicked ~callback:((fun () -> pet#bath; self#drawImage ((pet#get_sprite "bath")); self#refresh )));
-      ignore (button_kill#connect#clicked ~callback:((fun () -> pet#kill; self#drawImage ((pet#get_sprite "kill")); self#refresh )))
+
+
+    method anim  =
+      ignore (GMain.Timeout.add ~ms:3000 ~callback: self#end_anim); 
+
+    method end_anim () =
+      self#drawImage (pet#get_sprite "current");
+      _action <- false;
+      false
 
 
     method createPetZone =
@@ -75,16 +83,10 @@ class ui (pet: Pet.pet) =
 
     method drawImage link = 
       petImage#clear ();
-      (* petImage <- GMisc.image ~file:link ~packing:pbox#add ~width:0 ~show:true () *)
-
       petImage <- GMisc.image ~file:link ~packing:pbox#add  ~show:true ()
 
     method refresh=
-      if pet#is_dead
-        then
-          begin
-              self#destroy ()
-          end
+      if pet#is_dead then self#destroy ()
       else
         begin
             stat_health#set_fraction  ((float_of_int pet#get_health)  /. 100.);
